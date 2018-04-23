c889febd04f7bf7e69d3d6d6e9f8974ae64720a5
Dale Poulter <poulterd@gmail.com>
2018-04-23 08:16:05 -0500

    New 01van

---
+++ primo-explore/custom/01VAN_INST-vandev/js/custom.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+    'use strict';
+
+
+    var app = angular.module('viewCustom', ['angularLoad']);
+
+app.component('prmSearchBarAfter', {
+    template: `<span style="margin-left: 40%;">Hello World</span>`
+
+})
+
+})
+
+   
