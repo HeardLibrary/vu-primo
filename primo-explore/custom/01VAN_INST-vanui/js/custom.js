6da458c5cd0d24cdc22e78457ab513a9ab928549
Dale Poulter <poulterd@gmail.com>
2018-06-28 18:19:02 -0500

    change location and class of the forms for speccol

--- primo-explore/custom/01VAN_INST-vanui/js/custom.js
+++ primo-explore/custom/01VAN_INST-vanui/js/custom.js
@@ -5,7 +5,7 @@
 
 
 
-    var app = angular.module('viewCustom', ['angularLoad']);
+    var app = angular.module('viewCustom', ['angularLoad','customActions']);
 
 /** Show development environment **/ 
 /**
@@ -17,109 +17,229 @@ app.component('prmSearchBarAfter', {
 /** End show development environment **/
 
 
-/** Custom SMS **/
-/** add SMS button. Display only if a call number is available **/
-/**
-    app.component('prmSearchResultAvailablityLineAfter', {
-		bindings: { parentCtrl: '<' },
-		controller: 'smsController',
-		template: `
-			
-				<button class="md-icon-button custom-button md-button md-primoExplore-theme md-ink-ripple text-call-number-button" type="button" aria-label="Send call number for item via text" title="Text call number"  ng-if="$ctrl.parentCtrl.item.delivery.bestlocation.callNumber" ng-click="$ctrl.showDialog(ev);">
-					
-				Text Me
-				</button>
-		`
+/** Start Custom SMS **/
+
+/**  SMS form **/
+
+   app.component('prmActionListAfter', {
+	bindings: {parentCtrl: '<'},
+	controller: 'FormServiceController',
+	template: "<custom-action name=\"text_me\"\n                            label=\"Text Me\"\n                            index=0\n                            icon=\"ic_smartphone_24px\"\n                            icon-set=\"hardware\"\n                            link=\"https://library2018.library.vanderbilt.edu/forms/textme.php?call={{$ctrl.callNumber}}&location={{$ctrl.localCode}}&title={pnx.display.title}\" />"
     });
-
-**/
-
 
 
 /** End Custom SMS **/
 
+  
+
+
+/** basic form **/
+/**
+app.component('prmRequestServicesAfter',{
+	**/
+	app.component('prmLocationItemAfter',{
+    bindings: {parentCtrl: '<'},
+    controller: 'FormServiceController',
+	template: ' <span class="formbutton"> \
+	<form ng-if="$ctrl.localCode === \'ANNEX\'"  method="post" action="https://www.library.vanderbilt.edu/forms/custannex.php" target="_blank"> \
+	<input type="hidden" name="lang" value="en_US">\
+    <input type="hidden" value="{{$ctrl.title}}" name="title">\
+    <input type="hidden" value="{{$ctrl.localName}}" name="location">\
+    <input type="hidden" value="{{$ctrl.callNumber}}" name="call"> \
+	<input type="submit" name="submit" value="Request from Annex"> </form> \
+	<form ng-if="$ctrl.localCode === \'SPEC-COLL\'"  method="post" action="https://www.library.vanderbilt.edu/forms/archives.php" target="_blank"> \
+	<input type="hidden" name="lang" value="en_US">\
+    <input type="hidden" value="{{$ctrl.title}}" name="title">\
+    <input type="hidden" value="{{$ctrl.localName}}" name="location">\
+    <input type="hidden" value="{{$ctrl.callNumber}}" name="call"> \
+	<input type="submit" name="submit" value="Request for Use in the Reading Room"> </form> \
+	<form ng-if="$ctrl.localCode === \'21NORTH\'"  method="post" action="https://www.library.vanderbilt.edu/forms/archives.php" target="_blank"> \
+	<input type="hidden" name="lang" value="en_US">\
+    <input type="hidden" value="{{$ctrl.title}}" name="title">\
+    <input type="hidden" value="{{$ctrl.localName}}" name="location">\
+    <input type="hidden" value="{{$ctrl.callNumber}}" name="call"> \
+	<input type="submit" name="submit" class="formbutton" value="Request for Use in the Reading Room"> </form> </span> \
+	'});
+
 	
-/** Start Grinnel **/
+	app.controller('FormServiceController', [function () {
+    var vm = this;
+    vm.url = document.location || '';
+    var pnx = vm.parentCtrl.item.pnx || false;
+    vm.callNumber = vm.parentCtrl.item.delivery.bestlocation.callNumber ;
+    vm.localName = vm.parentCtrl.item.delivery.bestlocation.mainLocation;
+	vm.localCode = vm.parentCtrl.item.delivery.bestlocation.libraryCode;
+    vm.format = pnx.display.type[0] || '';
+    /** if(vm.format === 'article'){
+        vm.source = pnx.display.ispartof[0]+' by '+pnx.addata.au[0];
+    } else {
+        vm.source = 'Published by '+pnx.addata.pub[0]+' and authored by '+pnx.addata.au[0]+' in '+pnx.addata.date[0];
+    }
+	**/
+    vm.title = pnx.display.title[0] || '';
+    vm.url = document.location || '';
+    vm.$onInit = function () {
+    }
+}]);
+
+/** End basic Form **/
+
+
 
 
 	
 	
+	
+	
 
-    app.controller('smsController', ['angularLoad', '$scope', '$mdDialog', '$sce', '$http', function(angularLoad, $scope, $mdDialog, $sce, $http) {
-        this.showDialog = function(ev) {
-			var author;
-            if (this.parentCtrl.item.pnx.addata.au !== undefined) {
-                author = this.parentCtrl.item.pnx.addata.au[0];
-            } else {
-                author = '';
+	
+/** Start Orbis **/
+
+/**Start custom actions **/
+
+'use strict';
+
+angular.module('customActions', []);
+
+/* eslint-disable max-len */
+angular.module('customActions').component('customAction', {
+  bindings: {
+    name: '@',
+    label: '@',
+    icon: '@',
+    iconSet: '@',
+    link: '@',
+    index: '<'
+  },
+  require: {
+    prmActionCtrl: '^prmActionList'
+  },
+  controller: ['customActions', function (customActions) {
+    var _this = this;
+
+    this.$onInit = function () {
+      _this.action = {
+        name: _this.name,
+        label: _this.label,
+        index: _this.index,
+        icon: {
+          icon: _this.icon,
+          iconSet: _this.iconSet,
+          type: 'svg'
+        },
+        onToggle: customActions.processLinkTemplate(_this.link, _this.prmActionCtrl.item)
+      };
+      customActions.addAction(_this.action, _this.prmActionCtrl);
+    };
+    this.$onDestroy = function () {
+      return customActions.removeAction(_this.action, _this.prmActionCtrl);
+    };
+  }]
+});
+
+/* eslint-disable max-len */
+
+angular.module('customActions').factory('customActions', function () {
+  return {
+    /**
+     * Adds an action to the actions menu, including its icon.
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     */
+    // TODO coerce action.index to be <= requiredActionsList.length
+    addAction: function addAction(action, ctrl) {
+      if (!this.actionExists(action, ctrl)) {
+        this.addActionIcon(action, ctrl);
+        ctrl.actionListService.requiredActionsList.splice(action.index, 0, action.name);
+        ctrl.actionListService.actionsToIndex[action.name] = action.index;
+        ctrl.actionListService.onToggle[action.name] = action.onToggle;
+        ctrl.actionListService.actionsToDisplay.unshift(action.name);
+      }
+    },
+    /**
+     * Removes an action from the actions menu, including its icon.
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     */
+    removeAction: function removeAction(action, ctrl) {
+      if (this.actionExists(action, ctrl)) {
+        this.removeActionIcon(action, ctrl);
+        delete ctrl.actionListService.actionsToIndex[action.name];
+        delete ctrl.actionListService.onToggle[action.name];
+        var i = ctrl.actionListService.actionsToDisplay.indexOf(action.name);
+        ctrl.actionListService.actionsToDisplay.splice(i, 1);
+        i = ctrl.actionListService.requiredActionsList.indexOf(action.name);
+        ctrl.actionListService.requiredActionsList.splice(i, 1);
+      }
+    },
+    /**
+     * Registers an action's icon.
+     * Called internally by addAction().
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     */
+    addActionIcon: function addActionIcon(action, ctrl) {
+      ctrl.actionLabelNamesMap[action.name] = action.label;
+      ctrl.actionIconNamesMap[action.name] = action.name;
+      ctrl.actionIcons[action.name] = action.icon;
+    },
+    /**
+     * Deregisters an action's icon.
+     * Called internally by removeAction().
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     */
+    removeActionIcon: function removeActionIcon(action, ctrl) {
+      delete ctrl.actionLabelNamesMap[action.name];
+      delete ctrl.actionIconNamesMap[action.name];
+      delete ctrl.actionIcons[action.name];
+    },
+    /**
+     * Check if an action exists.
+     * Returns true if action is part of actionsToIndex.
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     * @return {bool}
+     */
+    actionExists: function actionExists(action, ctrl) {
+      return ctrl.actionListService.actionsToIndex.hasOwnProperty(action.name);
+    },
+    /**
+     * Process a link into a function to call when the action is clicked.
+     * The function will open the processed link in a new tab.
+     * Will replace {pnx.xxx.xxx} expressions with properties from the item.
+     * @param  {string}    link    the original link string from the html
+     * @param  {object}    item    the item object obtained from the controller
+     * @return {function}          function to call when the action is clicked
+     */
+    processLinkTemplate: function processLinkTemplate(link, item) {
+      var processedLink = link;
+      var pnxProperties = link.match(/\{(pnx\..*?)\}/g) || [];
+      pnxProperties.forEach(function (property) {
+        var value = property.replace(/[{}]/g, '').split('.').reduce(function (o, i) {
+          try {
+            var h = /(.*)(\[\d\])/.exec(i);
+            if (h instanceof Array) {
+              return o[h[1]][h[2].replace(/[^\d]/g, '')];
             }
-			var title;
-				title = this.parentCtrl.item.pnx.display.title[0];
-			var callnum;
-				callnum = this.parentCtrl.item.delivery.bestlocation.callNumber;
-			var mainloc;
-				mainloc = this.parentCtrl.item.delivery.bestlocation.mainLocation;
-				
-var dialogContent = '<md-dialog-content style="padding-top: 30px; padding-left: 30px; padding-right: 30px; padding-bottom: 10px;">' +
-				'<h3>' + this.parentCtrl.item.pnx.display.title[0] + '</h3>' +
-				'<h3>xxx' + callnum + 'xxxx</h3>' +
-				'<p>Send the title, location and call number of this item to your mobile phone.<br />' +
-				'<form name="this.textForm">' +
-				'<div layout="row" class="layout-row" style="padding-top:15px; padding-bottom:10px;"><div layout="row" layout-align="center" layout-fill class="layout-fill layout-align-center-stretch layout-row"><label for="phoneno">Enter your cell number:  </label> <input style=\"border-bottom: 1px solid rgba(0,0,0,.14);" type="text" name="phoneno" ng-model="this.phoneno" id="phoneno" size="10"></div></div>' +
-				'<div layout="row" class="layout-row"  style="padding-top:5px; padding-bottom:5px;"><div layout="row" layout-align="center" layout-fill class="layout-fill layout-align-center-stretch layout-row"><label for="provider">Select your provider:  </label> <select name="provider" ng-model="this.provider" style=\"border-top: none; border-left: none; border-right: none; border-bottom: 1px solid rgba(0,0,0,.14);">' +
-					'<option value="mms.att.net">AT&T</option>' +
-					'<option value="myboostmobile.com">Boost Mobile</option>' +
-					'<option value="mms.mycricket.com">Cricket</option>' +
-					'<option value="mymetropcs.com">MetroPCS</option>' +
-					'<option value="mypixmessages.com">Straight Talk</option>' +
-					'<option value="pm.sprint.com">Sprint</option>' +
-					'<option value="tmomail.net">T-Mobile</option>' +
-					'<option value="mms.uscc.net">U.S. Cellular</option>' +
-					'<option value="vzwpix.com">Verizon</option>' +
-					'<option value="vmpix.com">Virgin Mobile</option>' +
-				'</select></div></div>' +
-				'<input type="hidden"  name="title" value="' + title + '">' +
-				'<input type="hidden"  name="author" value="' + author + '">' +
-				'<input type="hidden"  name="callnum" value="' + callnum + '">' +
-				'<input type="hidden"  name="mainloc" value="' + mainloc + '">' +
-			    '</form></p></md-dialog-content>' +
-				'<md-dialog-actions>' +
-				'<md-button ng-click="sendText(title, author, mainloc,callnum)">SEND</md-button>' +
-				'<md-button ng-click="close()">CLOSE</md-button>' + /* This code gets parsed multiple times, and I couldn't get any normal method of escaping the single and double quotes to persist through all of the parsings. The PHP script that sends the text message replaces the qqqqqqqq and iiiiiiii strings with single and double quotes, respectively, before sending */
-				'</md-dialog-actions>'
-				;
-            dialogContent = $sce.trustAsHtml(dialogContent);
-            $mdDialog.show({
-				template: dialogContent,
-				clickOutsideToClose: true,
-				escapeToClose: true,
-				scope: angular.extend($scope.$new(), { 
-					close: function() {$mdDialog.cancel();},
-					sendText: function(title, author, mainloc, callnum) {
-						var message = {
-							method: 'POST',
-							url: 'https://apps.library.vanderbilt.edu/services/sms/send.php',
-							data: {
-								title: title,
-								author: author,
-								location: mainloc,
-								callnumber: callnum,
-								phoneno: this.phoneno,
-								provider: this.provider
-							}
-						}
-					$http(message).then(function(){console.log('text sent'); $mdDialog.cancel()});
-					}
-				}),
-			});
-		}
-	}]); 
+            return o[i];
+          } catch (e) {
+            return '';
+          }
+        }, item);
+        processedLink = processedLink.replace(property, value);
+      });
+	  
+      return function () {
+        return window.open(processedLink, '_blank');
+      };
+    }
+  };
+});
 
+/** End custom actions **/
 
-/** End Grinnnel **/	
-	
-	
-	
+/** End Orbis **/
 	
 	
 	
