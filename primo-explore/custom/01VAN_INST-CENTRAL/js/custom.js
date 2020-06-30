2618786a8b4e50589a4b7a1aef167416a2c21dca
Dale Poulter <poulterd@gmail.com>
2020-06-30 12:55:18 -0500

    updated package.json to correct security issues
    removed old packages and readded new packages.

--- primo-explore/custom/01VAN_INST-CENTRAL/js/custom.js
+++ primo-explore/custom/01VAN_INST-CENTRAL/js/custom.js
@@ -2,8 +2,8 @@
 "use strict";
 'use strict';
 
-var app = angular.module('viewCustom', ['angularLoad']);
-
+var app = angular.module('viewCustom', ['angularLoad','customActions','googleAnalytics']); 
+ 
 //Auto generated code by primo app store DO NOT DELETE!!! -START-
 /*
     hookName is a place holder with should hold the hook name not including "prm" at the beginning and in upper camel case
@@ -447,7 +447,7 @@ app.component('addMarcViewPrimoStudio', {
  (function() {
 
 	var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = 'true';
-//    lc.src =  'https://v2.libanswers.com/load_chat.php?hash=c58be3a8ecd194602bebd50fcfe6d49b';
+
 	lc.src = 'https://v2.libanswers.com/load_chat.php?hash=08328d78c3032c0ae5d5aa973c4b046c';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
     })();
@@ -498,8 +498,201 @@ angular.module('googleAnalytics').value('analyticsOptions', {
 });
 
 /** End Google Analytics **/
+
+/** Start custom actions **/
+
+
+app.component('prmActionListAfter', {
+	bindings: {parentCtrl: '<'},
+	controller: 'vuCustomActionsPrimoController',
+	template: `<custom-action name="text_me"
+                            label="Text Me"
+                            index=0
+                            icon="ic_smartphone_24px"
+                            icon-set="hardware"
+                            link="https://www.library.vanderbilt.edu/forms/textme.php?call={{$ctrl.getCall()}}&location={{$ctrl.getLibrary()}}&title={{$ctrl.getTitle()}}" />`
+/**            <custom-action  name="report_bug"
+                            label="Report Bug"
+                            index=7
+                            icon="ic_bug_report_24px"
+                            icon-set="action"
+                            link="https://www.library.vanderbilt.edu/report_problem?record_id={pnx.search.recordid[0]}" />`
+**/
+
+});
+
+
+app.controller('vuCustomActionsPrimoController', [function () {
+    
+     //set the context
+     var vm = this;
+    
+     //binds the function to the scope so it's requestable in the component.
+     vm.getRecordID = getRecordID;
+	 vm.getTitle = getTitle;
+	 vm.getCall = getCall;
+	 vm.getLibrary = getLibrary
+
+     //define the function that retrieves the record ID
+     function getRecordID() {
+         return vm.parentCtrl.item.pnx.control.recordid[0];
+     } 
+	 function getLibrary() {
+		 return vm.parentCtrl.item.delivery.bestlocation.mainLocation;
+	 } 
+	 function getTitle() {
+		return vm.parentCtrl.item.pnx.display.title[0]; 
+	 }
+	 function getCall() {
+		return vm.parentCtrl.item.delivery.bestlocation.callNumber;
+	 }	  
+}]);
+
 	
-	
+/** Start Orbis adapted code **/
+'use strict';
+
+angular.module('customActions', []);
+
+/* eslint-disable max-len */
+angular.module('customActions').component('customAction', {
+  bindings: {
+    name: '@',
+    label: '@',
+    icon: '@',
+    iconSet: '@',
+    link: '@',
+    index: '<'
+  },
+  require: {
+    prmActionCtrl: '^prmActionList'
+  },
+  controller: ['customActions', function (customActions) {
+    var _this = this;
+
+    this.$onInit = function () {
+      _this.action = {
+        name: _this.name,
+        label: _this.label,
+        index: _this.index,
+        icon: {
+          icon: _this.icon,
+          iconSet: _this.iconSet,
+          type: 'svg'
+        },
+        onToggle: customActions.processLinkTemplate(_this.link, _this.prmActionCtrl.item)
+      };
+      customActions.addAction(_this.action, _this.prmActionCtrl);
+    };
+    this.$onDestroy = function () {
+      return customActions.removeAction(_this.action, _this.prmActionCtrl);
+    };
+  }]
+});
+
+/* eslint-disable max-len */
+angular.module('customActions').factory('customActions', function () {
+  return {
+    /**
+     * Adds an action to the actions menu, including its icon.
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     */
+    // TODO coerce action.index to be <= requiredActionsList.length
+    addAction: function addAction(action, ctrl) {
+      this.addActionIcon(action, ctrl);
+      if (!this.actionExists(action, ctrl)) {
+        ctrl.actionListService.requiredActionsList.splice(action.index, 0, action.name);
+        ctrl.actionListService.actionsToIndex[action.name] = action.index;
+        ctrl.actionListService.onToggle[action.name] = action.onToggle;
+        ctrl.actionListService.actionsToDisplay.unshift(action.name);
+      }
+    },
+    /**
+     * Removes an action from the actions menu, including its icon.
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     */
+    removeAction: function removeAction(action, ctrl) {
+      if (this.actionExists(action, ctrl)) {
+        this.removeActionIcon(action, ctrl);
+        delete ctrl.actionListService.actionsToIndex[action.name];
+        delete ctrl.actionListService.onToggle[action.name];
+        var i = ctrl.actionListService.actionsToDisplay.indexOf(action.name);
+        ctrl.actionListService.actionsToDisplay.splice(i, 1);
+        i = ctrl.actionListService.requiredActionsList.indexOf(action.name);
+        ctrl.actionListService.requiredActionsList.splice(i, 1);
+      }
+    },
+    /**
+     * Registers an action's icon.
+     * Called internally by addAction().
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     */
+    addActionIcon: function addActionIcon(action, ctrl) {
+      ctrl.actionLabelNamesMap[action.name] = action.label;
+      ctrl.actionIconNamesMap[action.name] = action.name;
+      ctrl.actionIcons[action.name] = action.icon;
+    },
+    /**
+     * Deregisters an action's icon.
+     * Called internally by removeAction().
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     */
+    removeActionIcon: function removeActionIcon(action, ctrl) {
+      delete ctrl.actionLabelNamesMap[action.name];
+      delete ctrl.actionIconNamesMap[action.name];
+      delete ctrl.actionIcons[action.name];
+    },
+    /**
+     * Check if an action exists.
+     * Returns true if action is part of actionsToIndex.
+     * @param  {object} action  action object
+     * @param  {object} ctrl    instance of prmActionCtrl
+     * @return {bool}
+     */
+    actionExists: function actionExists(action, ctrl) {
+      return ctrl.actionListService.actionsToIndex.hasOwnProperty(action.name);
+    },
+    /**
+     * Process a link into a function to call when the action is clicked.
+     * The function will open the processed link in a new tab.
+     * Tbe pnx fields are requested but does not always work for VE recommend
+	 * defining in the customActionsPrimoController.
+     * @param  {string}    link    the original link string from the html
+     * @param  {object}    item    the item object obtained from the controller
+     * @return {function}          function to call when the action is clicked
+     */
+    processLinkTemplate: function processLinkTemplate(link, item) {
+      var processedLink = link;
+      var pnxProperties = link.match(/\{(pnx\..*?)\}/g) || [];
+      pnxProperties.forEach(function (property) {
+        var value = property.replace(/[{}]/g, '').split('.').reduce(function (o, i) {
+          try {
+            var h = /(.*)(\[\d\])/.exec(i);
+            if (h instanceof Array) {
+              return o[h[1]][h[2].replace(/[^\d]/g, '')];
+            }
+            return o[i];
+          } catch (e) {
+            return '';
+          }
+        }, item);
+        processedLink = processedLink.replace(property, value);
+      });
+      return function () {
+        return window.open(processedLink, '_blank');
+      };
+    }
+  };
+});
+
+
+/** End Orbis adapted code **/
+
+/** End Custom actions **/
 	
 
   
